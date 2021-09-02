2e3a91fb3cdd073e7bf1f89871066ea9d5ecf818
Emmanuel Stephen <emmanuel.r.stephen86@gmail.com>
2021-09-01 22:18:41 -0500

    feat: generating random new quote function

--- quote Generator/script.js
+++ quote Generator/script.js
@@ -1,12 +1,20 @@
 let apiQuotes = [];
 
+// Show new Quotes
+function newQuote(){
+    // generating a random quote
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log(quote);
+}
+
 // Get Quotes From API with an async function using a try catch statement
 async function getQuotes() {
     const apiUrl = 'https://type.fit/api/quotes';
     try{
         const response = await fetch(apiUrl);
         apiQuotes = await response.json();
-        console.log(apiQuotes);
+//        console.log(apiQuotes[12]);
+        newQuote();
     } catch (error) {
      // Catch Error Here
     }
